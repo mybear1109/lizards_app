1752172ebf8478f9e44663855eba54c80ce48ef9
mybear1109 <mybear8211@gmail.com>
2025-02-05 19:09:23 +0900

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,9 +1,12 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
 def render_sidebar():
     with st.sidebar:
         st.image("image/home_image.png", width=200)
 
         # ✅ 메뉴 생성
-        selected_option = option_menu( # type: ignore
+        selected_option = option_menu(
             menu_title="앱 탐색",
             options=["홈", "도마뱀 분석", "병원 검색", "유튜브 검색"],
             icons=["house", "image", "hospital", "youtube"],
