0aa3c255894e8bb2ba0b66b3363b13794d351759
mybear1109 <mybear8211@gmail.com>
2025-02-05 19:08:36 +0900

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,21 +1,14 @@
-import streamlit as st
-from streamlit_option_menu import option_menu
-
 def render_sidebar():
-    """
-    사이드바를 렌더링하는 함수
-    """
-    with st.sidebar:  # 사이드바 컨텍스트 내에서 렌더링
-        # 사이드바 상단 이미지 추가
-        st.image("image/home_image.png", width=300)
+    with st.sidebar:
+        st.image("image/home_image.png", width=200)
 
         # ✅ 메뉴 생성
-        selected_option = option_menu(
-            menu_title="앱 탐색",  # 메뉴 제목
-            options=["홈", "병원 검색", "유튜브 검색"],  # 메뉴 항목
-            icons=["house", "hospital", "youtube"],  # FontAwesome 아이콘
-            menu_icon="cast",  # 상단 메뉴 아이콘
-            default_index=0,  # 기본 선택 항목
+        selected_option = option_menu( # type: ignore
+            menu_title="앱 탐색",
+            options=["홈", "도마뱀 분석", "병원 검색", "유튜브 검색"],
+            icons=["house", "image", "hospital", "youtube"],
+            menu_icon="cast",
+            default_index=0,
             styles={
                 "container": {"padding": "5px", "background-color": "#f8f9fa"},
                 "icon": {"font-size": "20px"},
