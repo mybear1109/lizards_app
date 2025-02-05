4e20b9a6dbe7f48b03a0e57ba49689bf71530efc
mybear1109 <mybear8211@gmail.com>
2025-02-05 17:08:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,9 @@ def display_home():
     with col1:
         # 이미지 파일 경로 설정
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        image_path = os.path.join(BASE_DIR, "image", "001.PNG") # type: ignore
+        image_path = os.path.join(BASE_DIR, "image", "001.PNG")
+
+        st.write(f"이미지 경로: {image_path}")  # 경로 디버깅 로그
         if os.path.exists(image_path):
             st.image(image_path, width=300, use_container_width=True)  # 홈 화면 이미지
         else:
