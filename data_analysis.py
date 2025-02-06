0ed8efac119f1d9f103e57a023d439f4cbf20020
mybear1109 <mybear8211@gmail.com>
2025-02-06 13:49:45 +0900

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt  # ✅ 데이터 시각화를 위한 Matplotlib 
 
 # ✅ 데이터 파일 경로
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(BASE_DIR, "data", "Lizards.csv")
+DATA_PATH = os.path.join(BASE_DIR, "model", "Lizards.csv")
 
 # ✅ 데이터 분석 페이지
 def display_data_analysis():
