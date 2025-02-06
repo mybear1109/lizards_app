d777c87e17b442fc0ae558ad55c4a3c2fb059028
mybear1109 <mybear8211@gmail.com>
2025-02-06 12:47:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from hospital_page import display_hospitals
 from youtube_page import display_youtube_videos
 from image_analysis import display_image_analysis
 from about import show_about
+sys.path.append(os.path.dirname(os.path.abspath(__file__))) # type: ignore
 from data_analysis import display_data_analysis
 
 
