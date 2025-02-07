8ced26aea6c51b2b68d7af10c0d38d8f80517f12
mybear1109 <mybear8211@gmail.com>
2025-02-07 17:19:43 +0900

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+from hospital_page import display_hospitals
+from youtube_page import display_youtube_videos
 
 def render_sidebar():
     with st.sidebar:
