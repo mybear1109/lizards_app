43b5649f964153d22fa370bd8ee5944104252b15
mybear1109 <mybear8211@gmail.com>
2025-02-09 20:53:46 +0900

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -52,8 +52,8 @@ def render_sidebar():
 
         # ✅ 검색창 (선택된 메뉴에 따라 표시)
         if selected_option == "병원 검색":
-            st.session_state["hospital_query"] = st.text_input(st.session_state.get("hospital_query", "파충류 동물병원"))
+            st.session_state["hospital_query"] = st.text_input("🔍 병원 검색", st.session_state.get("hospital_query", "파충류 동물병원"))
         elif selected_option == "유튜브 검색":
-            st.session_state["youtube_query"] = st.text_input( st.session_state.get("youtube_query", "파충류 사육 방법"))
+            st.session_state["youtube_query"] = st.text_input("📺 유튜브 검색", st.session_state.get("youtube_query", "파충류 사육 방법"))
 
     return selected_option
